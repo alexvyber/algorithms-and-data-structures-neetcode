e3f0e468504de09adc1ed6cf435b5380c9a7dd84
Alexey Sokolov <alexvyber@gmail.com>
2023-06-22 14:30:59 +0300

    finished 5 module

--- 13-quick-sort/code.js
+++ 13-quick-sort/code.js
@@ -1,30 +1,30 @@
 function quickSort(arr, s, e) {
-	if (e - s + 1 <= 1) {
-		return arr;
-	}
+  if (e - s + 1 <= 1) {
+    return arr
+  }
 
-	let pivot = arr[e];
-	let left = s; // pointer for left side
+  const pivot = arr[e]
+  const left = s // pointer for left side
 
-	// Partition: elements smaller than pivot on left side
-	for (let i = s; i < e; i++) {
-		if (arr[i] < pivot) {
-			let tmp = arr[left];
-			arr[left] = arr[i];
-			arr[i] = tmp;
-			left++;
-		}
-	}
+  // Partition: elements smaller than pivot on left side
+  for (let i = s; i < e; i++) {
+    if (arr[i] < pivot) {
+      let tmp = arr[left]
+      arr[left] = arr[i]
+      arr[i] = tmp
+      left++
+    }
+  }
 
-	// Move pivot in-between left & right sides
-	arr[e] = arr[left];
-	arr[left] = pivot;
+  // Move pivot in-between left & right sides
+  arr[e] = arr[left]
+  arr[left] = pivot
 
-	// Quick sort left side
-	quickSort(arr, s, left - 1);
+  // Quick sort left side
+  quickSort(arr, s, left - 1)
 
-	// Quick sort right side
-	quickSort(arr, left + 1, e);
+  // Quick sort right side
+  quickSort(arr, left + 1, e)
 
-	return arr;
+  return arr
 }
--- 18-binary-search-trees/code.js
+++ 18-binary-search-trees/code.js
@@ -1,21 +1,21 @@
 class TreeNode {
-	constructor(val) {
-		this.val = val;
-		this.left = null;
-		this.right = null;
-	}
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
 }
 
 function search(root, target) {
-	if (root == null) {
-		return false;
-	}
+  if (root == null) {
+    return false
+  }
 
-	if (target > root.val) {
-		return search(root.right, target);
-	} else if (target < root.val) {
-		return search(root.left, target);
-	} else {
-		return true;
-	}
+  if (target > root.val) {
+    return search(root.right, target)
+  } else if (target < root.val) {
+    return search(root.left, target)
+  } else {
+    return true
+  }
 }
